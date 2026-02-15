4bbe09f00123a8391f0d3674a9c058fd9662786d
matias839 <matiisandoval34@gmail.com>
2026-02-15 18:20:21 -0300

    Initial commit: AvaloniaApplication1 project setup

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AvaloniaApplication1.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private string _greeting = "Mi primer programa en Avalonia";
+    
+    private int _clickCount;
+
+    [RelayCommand]
+    private void ChangeText()
+    {
+        _clickCount++;
+        Greeting = $"Has clickeado {_clickCount} veces";
+    }
+}
