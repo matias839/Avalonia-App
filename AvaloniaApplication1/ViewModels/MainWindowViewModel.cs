ae786a66b42a179eda3453fd7d91d28f7b155e06
matias839 <matiisandoval34@gmail.com>
2026-02-15 18:33:30 -0300

    Project updated succefully.

--- AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
-﻿namespace AvaloniaApplication1.ViewModels;
+﻿using AvaloniaApplication1.Views;
+
+namespace AvaloniaApplication1.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting = "Mi primer programa en Avalonia";
+    private string _greeting = "Contador de clicks: ";
     
     private int _clickCount;
 
@@ -13,6 +15,12 @@ public partial class MainWindowViewModel : ViewModelBase
     private void ChangeText()
     {
         _clickCount++;
-        Greeting = $"Has clickeado {_clickCount} veces";
+        Greeting = $"Contador de clicks: {_clickCount}";
+        
+        if (_clickCount >= 100)
+        {
+            Greeting = "¡Has alcanzado el límite de clicks!";
+            _clickCount = 100;
+        }
     }
 }
--- AvaloniaApplication1/obj/Debug/net10.0/AvaloniaApplication1.AssemblyInfo.cs
+++ AvaloniaApplication1/obj/Debug/net10.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbe09f00123a8391f0d3674a9c058fd9662786d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
